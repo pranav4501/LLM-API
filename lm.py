e1917438b149eba60b56e75902af535c8117bbe8
Pranav <56645758+pranav4501@users.noreply.github.com>
2024-08-12 02:21:56 +0000

    groq inference

--- lm.py
+++ lm.py
@@ -5,13 +5,19 @@ import chromadb
 import vertexai
 from exa_py import Exa
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from openai import OpenAI
 from vertexai.generative_models import ChatSession, GenerativeModel
 from vertexai.language_models import TextEmbeddingModel
+from openai import AsyncOpenAI
+from groq import AsyncGroq
+
+
 
 from json_from_r import json_from_r
 
 my_sec = os.environ['EXA_API_KEY']
 exa = Exa(my_sec)
+groqKey = os.environ['GROQ_API_KEY']
 
 def get_vertex_embeddings(texts, embedding_model):
   # embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
@@ -25,7 +31,7 @@ def get_vertex_embeddings(texts, embedding_model):
 
   return all_embeddings
 
-def get_answer_from_llm(query, exa, embedding_model, client, model):
+def get_context(query, exa, embedding_model, client):
   # query = input()
   
   result = exa.search_and_contents(
@@ -99,18 +105,27 @@ def get_answer_from_llm(query, exa, embedding_model, client, model):
   # system_instruction=["You are a personal assistant.","You are given related information from the internet as context use if needed", "cite the source url when using the context"] # type: ignore
   # model = GenerativeModel(model_id_adv, system_instruction=system_instruction)  # type: ignore
   
-  print(context)
-  
-  
-  chat = model.start_chat()
-  # res2 = chat.send_message("user: " + query)
-  # print("\n" + res2.text )
-  
-  query_c = context + " user:" + query
-  res = chat.send_message(query_c )
-  print("\n" + res.text) #type:ignore
+  return context
 
-  return res.text
+async def get_answer_from_llm(query, exa, embedding_model, client, model):
+
+  context = get_context(query, exa, embedding_model, client)
+  
+  groq_client  = AsyncGroq(api_key=groqKey)
+  query_c = context + " user:" + query
+
+  stream = await groq_client.chat.completions.create(
+    model="llama3-8b-8192",
+    messages=[
+        {"role": "system", "content": "You are a personal assistant, answer the questions in 5 to 10 sentences. You are given related information from the internet as context. Cite the sources when using the context with markdown formating of url and title"},
+        {"role": "user", "content": query_c}
+    ],
+    stream=True,
+    stop = None
+  )
+  async for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+      yield chunk.choices[0].delta.content
 
 
 # my_sec = os.environ['EXA_API_KEY']
